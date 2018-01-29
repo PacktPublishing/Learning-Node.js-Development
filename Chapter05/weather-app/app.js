00679a828dfebea5ad2c970b5f08de4fc207e5aa
anupamt01 <33212178+anupamt01@users.noreply.github.com>
2018-01-29 20:13:19 +0530

    Add files via upload

---
+++ Chapter05/weather-app/app.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+request({
+  url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  json: true
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
