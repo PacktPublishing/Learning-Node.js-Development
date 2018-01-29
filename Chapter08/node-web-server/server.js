de736f0fcdf36c99d9f7156eafb058a6d44c9460
anupamt01 <33212178+anupamt01@users.noreply.github.com>
2018-01-29 20:14:50 +0530

    Add files via upload

---
+++ Chapter08/node-web-server/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  //return 'test';
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+      errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
---
+++ Chapter08/node-web-server/views/partials/header.hbs
@@ -0,0 +1,5 @@
+<header>
+<h1>{{pageTitle}}</h1>
+<p><a href="/">Home</a></p>
+<p><a href="/about">About</a></p>
+</header>
